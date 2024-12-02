87a40bf9d1f24ddc4b1a23acf3dfaffe8ef01050
Thabani Takwena <thabanidev@gmail.com>
2024-12-02 10:12:50 +0200

    tabs setup

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,21 +1,37 @@
 import React from "react";
 import { Tabs } from "expo-router";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function TabsLayout() {
 	return (
-		<Tabs>
+		<Tabs
+			screenOptions={{
+				headerShown: false,
+			}}
+		>
 			<Tabs.Screen
 				name="index"
 				options={{
 					title: "Home",
+					tabBarIcon: ({ color, size }) => (
+						<Ionicons name="home" color={color} size={size} />
+					),
 				}}
 			/>
-      <Tabs.Screen
-        name="about"
-        options={{
-          title: "About",
-        }}
-      />
+			<Tabs.Screen
+				name="about"
+				options={{
+					title: "About",
+					tabBarIcon: ({ color, size }) => (
+						<Ionicons
+							name="information"
+							color={color}
+							size={size}
+						/>
+
+					),
+				}}
+			/>
 		</Tabs>
 	);
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
 
 export default function Home() {
-  return (
-	<View>
-	  <Text>Home</Text>
-	</View>
-  )
+	return (
+		<View className="flex-1 items-center justify-center bg-sky-500">
+			<Text className="font-semibold text-2xl text-sky-950 mb-4">
+				Edit app/index.tsx to edit this screen.
+			</Text>
+
+			<TouchableOpacity
+				onPress={() => alert("Sup!")}
+				className="px-12 py-6 bg-sky-950 rounded-full"
+				activeOpacity={0.8}
+			>
+				<Text className="text-white text-xl">Get started</Text>
+			</TouchableOpacity>
+		</View>
+	);
 }
