ac0ee2e026a1f141495caf9dc106f31cf19b06bc
Thabani Takwena <thabanidev@gmail.com>
2024-12-02 10:01:43 +0200

    setting up tabs

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,15 @@
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   return (
     <View
-      className="flex-1 items-center justify-center"
+      className="flex-1 items-center justify-center bg-sky-500"
     >
-      <Text className="font-semibold text-2xl">Edit app/index.tsx to edit this screen.</Text>
+      <Text className="font-semibold text-2xl text-sky-950 mb-4">Edit app/index.tsx to edit this screen.</Text>
+
+      <TouchableOpacity onPress={() => alert("Sup!")} className="px-12 py-6 bg-sky-950 rounded-full" activeOpacity={0.8}>
+        <Text className="text-white text-xl">Get started</Text>
+      </TouchableOpacity>
     </View>
   );
 }
